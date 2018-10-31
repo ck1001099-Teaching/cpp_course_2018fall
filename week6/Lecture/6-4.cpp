31fa5ede19644d2adac8086f3f9f53f5d133992b
CHENYI <r07922059@ntu.edu.tw>
2018-10-31 16:49:20 +0800

    新增第六週上課教材

---
+++ week6/Lecture/6-4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int p1[10];
+	int *p1_ptr = new int[10];
+
+	int p2[10][20];
+	int **p2_ptr = new int*[10];
+	for (int i = 0 ; i < 10 ; i++){
+		p2_ptr[i] = new int[20];
+	}
+
+	for (int i = 0 ; i < 10 ; i++){
+		for (int j = 0 ; j < 20 ; j++){
+			p2_ptr[i][j] = i*20+j;
+			cout << "Value: " << p2_ptr[i][j] << endl;
+		}
+	}
+
+	return 0;
+}
